77a1c1b741f209e78015041c82b303a9cac04bae
AygunHajiyeva <aygunhajiyevaa@gmail.com>
2022-10-02 12:50:01 +0400

    created environment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import Quote from './Quote'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+    const [options, setOptions] = useState(null)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <Quote />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quote = () => {
+  return (
+    <div className='quote-container'>
+        <p>Random Quote</p>
+    </div>
+  )
+}
+
+export default Quote
